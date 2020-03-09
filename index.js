ef107f49b836a9c0e8fff25d440827c9ac39298f
Gagan Singh <randhawa_gagan@live.com>
2020-03-09 09:28:49 +0000

    array polyfill

--- index.js
+++ index.js
@@ -7,6 +7,34 @@ class MyArray{
   get(index){
     return this.data[index];
   }
+
+  push(item){
+    this.data[this.length] = item;
+    this.length++;
+     return this.length;
+  }
+  pop(){
+    const last = this.data[this.length - 1];
+    delete this.data[this.length -1];
+    this.length--;
+    return last
+  }
+  delete(index){
+    const item = this.data[index];
+    this.shiftItems(index);
+  }
+
+  shiftItems(index){
+    for(let i=index; i< this.length - 1 ;i++){
+      this.data[i] = this.data[i+1];
+    }
+    delete this.data[this.length - 1]
+    this.length--;
+  }
 }
 const myarr = new MyArray();
+myarr.push("gagan")
+myarr.push("jot")
+myarr.push("randhawa")
+myarr.delete(1);
 console.log(myarr)
