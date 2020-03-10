6bddcbfd8bacaf9c68c5c6fac42bd0c36d972aa2
Gagan Singh <randhawa_gagan@live.com>
2020-03-10 06:21:08 +0000

    hash table implementafaion

--- index.js
+++ index.js
@@ -1,6 +1,41 @@
  /* Hash Tables  */
+ class HashTable{
+  constructor(size){
+    this.data = new Array(size);
+  }
+   _hash(key) {
+    let hash = 0;
+    for(let i=0; i<key.length;i++){
+        hash = (hash + key.charCodeAt(i) * i) % this.data.length
+    }
+    return hash;
+    }
+    get(key){
+      let address = this._hash(key);
+      const currentBucket = this.data[address];
 
-
+      for(let i=0; i<currentBucket.length ; i++){
+        if(currentBucket[i][0] === key){
+          return currentBucket[i][1];
+        }
+        return undefined
+      }
+    }
+    set(key, value){
+      let address = this._hash(key);
+      if(!this.data[address]){
+        this.data[address] = [];
+      }
+      this.data[address].push([key,value])
+      return this.data;
+    }
+  }
+const myHash = new HashTable(50);
+myHash._hash("gagsan");
+myHash.set('grapes', 100);
+myHash.set('g', 1003333);
+myHash.get('grapes');
+myHash.get('g');
 
  /* Merge Sorted arrays */
 
