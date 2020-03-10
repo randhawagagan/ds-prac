a4e6030ec2243b7348613f52f233ace8be86715e
Gagan Singh <randhawa_gagan@live.com>
2020-03-10 08:17:03 +0000

    Google question forst recurring character

--- index.js
+++ index.js
@@ -26,11 +26,7 @@ function firstRecurringCharacter(input){
     }
 }
 const array = [2,5,1,2,3,5,1,2,4];
-console.log(firstRecurringCharacter([2,1,1,2,3,5,1,2,4]))
-
-//Bonus... What if we had this:
-// [2,5,5,2,3,5,1,2,4]
-// return 5 because the pairs are before 2,2
+console.log(firstRecurringCharacter([2,5,5,2,3,5,1,2,4]))
 
 
 
